ee1561ce0842e9f0abbfcea9ae029cbe139070f1
Sergii <hello@webkadabra.com>
2015-09-25 16:54:11 +0300

    add wiki

--- common/src/app/mapDispatchToProps.js
+++ common/src/app/mapDispatchToProps.js
@@ -1,11 +1,13 @@
 import * as authActions from '../auth/actions';
 import * as todosActions from '../todos/actions';
+import * as wikiActions from '../wiki/actions';
 import {Map} from 'immutable';
 import {bindActionCreators} from 'redux';
 
 const actions = [
   authActions,
-  todosActions
+  todosActions,
+  wikiActions
 ];
 
 export default function mapDispatchToProps(dispatch) {
--- common/src/app/reducer.js
+++ common/src/app/reducer.js
@@ -6,13 +6,15 @@ import device from '../device/reducer';
 import intl from '../intl/reducer';
 import todos from '../todos/reducer';
 import users from '../users/reducer';
+import wiki from '../wiki/reducer';
 
 const appReducer = combineReducers({
   auth,
   device,
   intl,
   todos,
-  users
+  users,
+  wiki
 });
 
 export default appReducer;
