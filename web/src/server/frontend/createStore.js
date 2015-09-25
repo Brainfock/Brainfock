98dc8d1f119276f1e14168bb7d00c1b55b69684b
Sergii <hello@webkadabra.com>
2015-09-25 03:33:43 +0300

    Persist user auth state to client

--- web/src/server/frontend/createStore.js
+++ web/src/server/frontend/createStore.js
@@ -4,14 +4,29 @@ import initialState from '../initialState';
 import {configureStore} from '@este/common';
 import {fromJS} from 'immutable';
 import {mapDispatchToProps} from '@este/common';
+import loopback from 'loopback';
 
 export default function createStore(req) {
   return new Promise((resolve, reject) => {
-    const requestState = fromJS(initialState).mergeDeep({
+
+    var ctx = loopback.getCurrentContext();
+    var currentUser = ctx && ctx.get('currentUser');
+
+    var state = {
       device: {
         isMobile: ['phone', 'tablet'].indexOf(req.device.type) > -1
+      },
+      users:{
+        viewer: currentUser ? ctx.get('currentUser') : null,
       }
-    });
+    };
+
+    // provide auth token id to client
+    if(currentUser) {
+      state.users.viewer.authToken = ctx.get('accessToken').id;
+    }
+
+    const requestState = fromJS(initialState).mergeDeep(state);
     const store = configureStore(requestState.toJS());
     const {actions} = mapDispatchToProps(store.dispatch);
 
