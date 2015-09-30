47ebe9522338dba1f9d4f2b985ed5994044ddf45
Sergii <hello@webkadabra.com>
2015-09-30 13:32:05 +0300

    production datasources config

---
+++ web/src/server/datasources.production.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db: {
+    connector: 'mysql',
+    hostname: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 0,
+    user: process.env.DB_USER || 'brainfock',
+    password: process.env.DB_PASSWORD || 'password',
+    database: 'brainfock_node',
+  }
+};
