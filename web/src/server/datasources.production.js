186927603c16e840c47c6fe93094798774e9b987
Sergii <hello@webkadabra.com>
2015-09-30 16:45:26 +0300

    testing configs resolving on demo server

--- web/src/server/datasources.production.js
+++ web/src/server/datasources.production.js
@@ -1,10 +1,10 @@
 module.exports = {
   db: {
     connector: 'mysql',
-    hostname: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '178.62.249.90',
     port: process.env.DB_PORT || 0,
     user: process.env.DB_USER || 'brainfock',
     password: process.env.DB_PASSWORD || 'password',
-    "database": 'non-existing',
+    database: 'non-existing',
   }
 };
