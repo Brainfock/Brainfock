a69cafee6f5fb798351e34ca62583c31905f6481
Sergii <hello@webkadabra.com>
2015-09-30 16:36:30 +0300

    testing configs resolving on demo server

--- web/src/server/boot/root.js
+++ web/src/server/boot/root.js
@@ -2,6 +2,20 @@ import frontend from '../frontend';
 
 module.exports = function(server) {
   var restApiRoot = server.get('restApiRoot');
+
   server.use(restApiRoot, server.loopback.rest());
+
+  server.get('/status', function(req, res) {
+    let baseUrl = server.get('url').replace(/\/$/, '');
+
+    res.send({
+      pong:Date.now(),
+      baseUrl:baseUrl,
+      host:server.get('host'),
+      port:server.get('port'),
+      publicHost:server.get('publicHost'),
+    });
+  });
+
   server.use(frontend);
 };
