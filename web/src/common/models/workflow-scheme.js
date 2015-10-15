6cccff67e145c0a2f79d3f097326143f94afe031
Sergii Gamaiunov <hello@webkadabra.com>
2015-10-15 04:58:38 +0300

    Cleanup - `getDefaultStage` helper is available in `Workflow` model,  not `WorkflowScheme`

--- web/src/common/models/workflow-scheme.js
+++ web/src/common/models/workflow-scheme.js
@@ -1,16 +1,3 @@
 module.exports = function(WorkflowScheme) {
-  WorkflowScheme.prototype.getDefaultStage = function(next) {
-    WorkflowScheme.app.models.WorkflowStage.findOne({where: {
-      workflowId: this.id,
-      isInitial: 1,
-    }}, function(err, wfStage) {
 
-      if(err) throw err;
-
-      if(!wfStage)
-        next(null, null)
-      else
-        next(null, wfStage)
-    })
-  }
 };
