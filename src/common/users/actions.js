31359c6118b4201ff94e680371accdcbd8381c29
Sergii Gamaiunov <hello@webkadabra.com>
2015-10-26 02:20:37 +0200

    Add `users` actions

---
+++ src/common/users/actions.js
@@ -0,0 +1,50 @@
+/**
+ * Brainfock - community & issue management software
+ * Copyright (c) 2015, Sergii Gamaiunov (“Webkadabra”)  All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.brainfock.com/
+ * @copyright Copyright (c) 2015 Sergii Gamaiunov <hello@webkadabra.com>
+ */
+import {apiGet, apiPost} from '../lib/services';
+import {toQueryString} from '../utils/model.js';
+
+export const FIND = 'FIND_USERS';
+export const FIND_SUCCESS = 'FIND_USERS_SUCCESS';
+export const FIND_ERROR = 'FIND_USERS_ERROR';
+
+export function findUsers(query) {
+  let endpoint = '';
+
+  endpoint += 'members/';
+
+  if(query) {
+    endpoint += '?'+toQueryString({filter:{where:query}},false);
+  }
+
+  return ({fetch, validate}) => ({
+    type: [
+      FIND,
+      FIND_SUCCESS,
+      FIND_ERROR
+    ],
+    payload: {
+      promise:  apiGet(fetch, endpoint)
+        .catch(response => {
+          throw response;
+        })
+    }
+  });
+}
