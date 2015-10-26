cf17d6d098b220dbba3251f1fadbbf0f3acdf2c2
Sergii Gamaiunov <hello@webkadabra.com>
2015-10-26 23:40:48 +0200

    Modify `stateToJs` to allow non-immutable data for `form` (redux-form) only

--- src/common/lib/stateToJS.js
+++ src/common/lib/stateToJS.js
@@ -1,5 +1,9 @@
 export default function stateToJS(state) {
   return Object.keys(state).reduce((acc, key) => {
-    return {...acc, [key]: state[key].toJS()};
+    // allowing non-immutable data for `form` (redux-form) only
+    const data = (!state[key].toJS && key === 'form')
+      ? state[key]
+      : state[key].toJS();
+    return {...acc, [key]: data};
   }, {});
 }
