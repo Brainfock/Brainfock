a2b8d67e11cd0ca80a4ea0ae67bfe70d0ea82882
Sergii Gamaiunov <hello@webkadabra.com>
2015-10-28 19:31:21 +0200

    Loader component: Add option to render line-progress bar only, without `loading` label

--- src/client/components/Loader.js
+++ src/client/components/Loader.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React , {PropTypes} from 'react';
 import {LinearProgress} from 'material-ui';
 
 // TODO: i18n
 module.exports = React.createClass({
+  propTypes: {
+    noLabel: PropTypes.bool
+  },
   render: function() {
+    let propgressBarStyle = {}
+    if (!this.props.noLabel) {
+      propgressBarStyle = {marginTop: '5%'}
+    }
     return (
-      <div style={{textAlign:'center',marginTop:'10%'}}>
-        <i className="fa fa-cog fa-spin" ></i> Loading...
+      <div style={{textAlign:'center',marginTop:!this.props.noLabel ? '10%' : 0}}>
+        {!this.props.noLabel && <span><i className="fa fa-cog fa-spin"></i> Loading...</span>}
 
-        <LinearProgress mode="indeterminate" style={{marginTop:'5%'}} />
+        <LinearProgress mode="indeterminate" style={propgressBarStyle}/>
       </div>
     );
   }
