b723a86ef22aeba8192d26dd4ebf12a124e6e113
Sergii Gamaiunov <hello@webkadabra.com>
2015-11-01 04:03:31 +0200

    Add handling of errors of 'createTopic' form

--- src/common/form/reducer.js
+++ src/common/form/reducer.js
@@ -9,10 +9,13 @@ import {createStore, combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 
 import {CREATE_ERROR} from '../workspace/actions';
+import {SET_NEW_TOPIC_FIELD} from '../topics/actions';
+import {CREATE_ERROR as TOPIC_CREATE_ERROR} from '../topics/actions';
 
 module.exports =  formReducer.plugin({
+  // `createWorkspace` is the name of form
   createWorkspace: (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
       case CREATE_ERROR:
         if (action.error === true) {
           if (action.payload.error && action.payload.error.details) {
@@ -38,6 +41,79 @@ module.exports =  formReducer.plugin({
         } else {
           return state;
         }
+      default:
+        return state;
+    }
+  },
+  createTopic: (state, action) => {
+    switch (action.type) {
+      case TOPIC_CREATE_ERROR:
+        if (action.error === true) {
+          console.log(' >>>', action)
+          if (action.payload.error && action.payload.error.details) {
+
+            // loop
+            for (let fieldName in action.payload.error.details.messages) {
+              if (action.payload.error.details.messages.hasOwnProperty(fieldName)) {
+                const message = action.payload.error.details.messages[fieldName];
+                if (!state[fieldName]) {
+                  state[fieldName] = {};
+                }
+                state[fieldName].asyncError = message.join('; ');
+                state[fieldName].touched = true;
+              }
+              ;
+            }
+            return { //
+              ...state,
+            };
+            return {
+              ...state,
+              namespace: {
+                asyncError: action.payload.error.details.messages.namespace.join('; '),
+                touched: true,
+                value: state.namespace.value
+              }
+            };
+          } else if (action.payload.error) {
+            return { //
+              ...state,
+              _error: action.payload.error.message || 'Unknown Error!'
+            };
+          } else {
+            return {
+              ...state,
+              _error: action.payload.message.length > 0 && action.payload.message || 'Unknown Error!'
+            };
+          }
+        } else {
+          return state;
+        }
+      case SET_NEW_TOPIC_FIELD:
+      {
+        console.log('> redux-form plugin:', action, state)
+        if (state[action.payload.name]) {
+          state[action.payload.name].value = action.payload.value;
+        } else {
+          state[action.payload.name] = {value: action.payload.value};
+        }
+        // reset error when we reset form value
+        state[action.payload.name].error = null;
+        //state[action.payload.name].asyncError = null;
+        state[action.payload.name].touched = true;
+        return state;
+        //return {
+        //  ...state,
+        //  namespace: {
+        //    asyncError: action.payload.error.details.messages.namespace.join('; '),
+        //    touched: true,
+        //    value: state.namespace.value
+        //  }
+        //};
+
+        //const {name, value} = action.payload;
+        //return state.setIn(['newTopic', name], value);
+      }
       default:
         return state;
     }
