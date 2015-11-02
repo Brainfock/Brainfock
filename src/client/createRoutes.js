19d5d2111e8db83164e7008e4643cee45d18dfc9
Sergii Gamaiunov <hello@webkadabra.com>
2015-11-02 04:18:01 +0200

    Add topic board topi route when no slug available

--- src/client/createRoutes.js
+++ src/client/createRoutes.js
@@ -36,6 +36,7 @@ export default function createRoutes(getState) {
       <Route component={require('./boards/pages/board.js')} path="/board/:board_id/:key">
         { /*<Route component={require('./boards/pages/topic.js')} path="/board/:board_id/topic/:id" /> */ }
       </Route>
+      <Route component={require('./boards/pages/topic.js')} path="/board/:board_id/:boardKey/:id"/>
       <Route component={require('./boards/pages/topic.js')} path="/board/:board_id/:boardKey/:id/:key"/>
 
       <Route component={Wiki} path="wiki/:uid" />
