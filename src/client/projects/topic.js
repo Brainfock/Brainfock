c85d87ecb4c6bb67a57b06e77136df832fd45227
Sergii Gamaiunov <hello@webkadabra.com>
2015-11-02 16:27:02 +0200

    Bump version

--- src/client/projects/topic.js
+++ src/client/projects/topic.js
@@ -1,21 +1,11 @@
 /**
- * Brainfock - community & issue management software
+ * Brainfock - business & community management software
  * Copyright (c) 2015, Sergii Gamaiunov (“Webkadabra”)  All rights reserved.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @link http://www.brainfock.com/
+ * @link http://www.brainfock.org/
  * @copyright Copyright (c) 2015 Sergii Gamaiunov <hello@webkadabra.com>
  */
 var React = require('react');
@@ -26,6 +16,7 @@ var bs = require('react-bootstrap'),
 var Loader = require('../components/Loader');
 var AppContentCanvas = require('../components/layout/AppContentCanvas');
 import OperationsDropdown from './components/OperationsDropdown.js'
+
 /**
  * TopicView
  *
