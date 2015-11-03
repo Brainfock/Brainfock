cf77d5cf4851092fd6bd921d2ed78a073c903ec5
Sergii Gamaiunov <hello@webkadabra.com>
2015-11-03 23:52:40 +0200

    Deleting topic: clean up all topics in that [topic's] context, recursively
    Fixes #20Feature: delete projects

--- src/common/models/topic-raw.js
+++ src/common/models/topic-raw.js
@@ -1,14 +1,26 @@
 module.exports = function(RawTopic) {
 
-  //RawTopic.observe('after delete', function(ctx, next) {
-  //
-  //  RawTopic.find(ctx.where, (err, data) => {
-  //    // TODO: clean up comment, entity etc.?
-  //    console.log('Deleted %s matching %j, %j, %j, %j',
-  //      ctx.Model.pluralModelName,
-  //      ctx.where
-  //    );
-  //  })
-  //  next();
-  //});
-}
+  RawTopic.observe('before delete', function(ctx, next) {
+
+    RawTopic.find({where:ctx.where}, (err, topics) => {
+
+      if (err) next(err);
+
+      let resCount = topics.length;
+      let lopRes = [];
+      if (resCount > 0) {
+        topics.forEach(function(item) {
+
+          RawTopic.destroyAll({contextTopicId: item.id}, () => {
+            lopRes.push(1);
+            if (lopRes.length === (resCount)) {
+              next();
+            }
+          });
+        });
+      } else {
+        next();
+      }
+    });
+  });
+};
