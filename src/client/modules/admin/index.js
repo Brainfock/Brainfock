56b538e6c32e5c7eed093f2ac19190a0df839e3c
Sergii Gamaiunov <hello@webkadabra.com>
2015-11-08 01:53:45 +0200

    Hotfix: add missing file for admin module

---
+++ src/client/modules/admin/index.js
@@ -0,0 +1,30 @@
+/**
+ * Admin module router configuration
+ *
+ * @category client/modules/admin
+ * @type {{path: string, getComponent, getChildRoutes}}
+ */
+module.exports = {
+  path: 'admin',
+
+  getComponent(location, cb) {
+    if (process.env.IS_BROWSER) {
+      require.ensure([], (require) => {
+        cb(null, require('./pages/AdminHome'))
+      })
+    } else {
+      cb(null, require('./pages/AdminHome'));
+    }
+  },
+
+  getChildRoutes(location, cb) {
+    if (process.env.IS_BROWSER)
+      require.ensure([], (require) => {[
+          cb(null, require('./modules/users'))
+      ]})
+    else
+      cb(null, [
+        require('./modules/users'),
+      ]);
+  },
+}
