e92648f02b151fd732d28b9b9d0ac83bdb7013f8
Sergii Gamaiunov <hello@webkadabra.com>
2015-11-07 20:04:11 +0200

    Modify `FeatchActionError` component to use Material design gidelines for error

--- src/client/components/FetchActionError.js
+++ src/client/components/FetchActionError.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import mui from 'material-ui';
+import mui, {Card, CardHeader, CardTitle, CardActions, FlatButton, CardText} from 'material-ui';
 import Component from 'react-pure-render/component';
 
 export default class FetchActionError extends Component {
@@ -29,9 +29,32 @@ export default class FetchActionError extends Component {
     }
 
     return (
+      <div style={this.getWrapperStyle()}>
+        <CardText>{message}</CardText>
+        <CardActions style={{paddingTop:0}}>
+          <FlatButton
+            onClick={this.props.handleRetry}
+            label="Retry"
+            style={{padding:100}}
+            labelStyle={{color:'#B95252'}}
+            />
+        </CardActions>
+      </div>
+    )
+    return (
       <div className="alert alert-warning" style={{margin:0,borderRadius:0}}>{message}
         <button onClick={this.props.handleRetry}>Retry</button>
       </div>
     )
+  }
+
+  getWrapperStyle() {
+    return {
+    //  -webkit-box-shadow: inset 0px 7px 8px -4px rgba(92,92,92,0.52);
+    //-moz-box-shadow: inset 0px 7px 8px -4px rgba(92,92,92,0.52);
+      boxShadow: 'inset 0px 7px 8px -4px rgba(92,92,92,0.45)',
+      background: '#efefef',
+      color: 'maroon'
+    }
   }
 };
