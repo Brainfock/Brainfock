ba9c09d3542016fbb7480280961c445bfdf2548f
Sergii Gamaiunov <hello@webkadabra.com>
2015-11-09 06:27:42 +0200

    Add immutable record for user profile update form

---
+++ src/common/users/changePassword.form.js
@@ -0,0 +1,24 @@
+/**
+ * Brainfock - Community & Business Management Solution
+ *
+ * @link http://www.brainfock.org
+ * @copyright Copyright (c) 2015 Sergii Gamaiunov <hello@webkadabra.com>
+ */
+
+import {Record, Map} from 'immutable';
+
+export default Record({
+  cid: '',
+  data: new  (Record({
+    password: '',
+    confirmPassword: '',
+    currentPassword: '',
+    newEmail: '',
+  })),
+  meta: new (Record({
+    isSubmitting: false,
+    postedOn: '',
+    error: '',
+    errors: new Map()
+  }))
+});
