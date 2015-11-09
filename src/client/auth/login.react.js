263a5f8623230f17eede41c653a0703b67ed291c
Sergii Gamaiunov <hello@webkadabra.com>
2015-11-09 06:49:45 +0200

    Tweak login form UI adding more contrast

--- src/client/auth/login.react.js
+++ src/client/auth/login.react.js
@@ -2,7 +2,7 @@ import Component from 'react-pure-render/component';
 import React, {PropTypes} from 'react';
 import focusInvalidField from '../lib/focusInvalidField';
 import {Grid, Row, Col} from 'react-bootstrap';
-import mui from 'material-ui';
+import mui, {Paper} from 'material-ui';
 
 if (process.env.IS_BROWSER)
   require('./login.styl');
@@ -43,37 +43,45 @@ export default class Login extends Component {
     const {actions, auth: {form}, msg: {auth: {form: msg}}} = this.props;
 
     return (
-      <div className="login" style={{marginTop:20}}>
+      <div className="login" style={{paddingTop:20}}>
         <Grid>
-          <Row className="show-grid">
-            <Col sm={8} smPush={4} xs={12}>
-              <form onSubmit={::this.onFormSubmit}>
-                <fieldset disabled={form.disabled}>
-                  <legend>{msg.legend}</legend>
-                  <mui.TextField
-                    autoFocus
-                    hintText={msg.placeholder.email}
-                    name="email"
-                    onChange={actions.setFormField}
-                    value={form.fields.email}/>
-                  <br />
-                  <mui.TextField
-                    hintText={msg.placeholder.password}
-                    name="password"
-                    onChange={actions.setFormField}
-                    type="password"
-                    value={form.fields.password}/>
-                  <br />
-                  <mui.RaisedButton
-                    disabled={form.disabled}
-                    label={msg.button.login}
-                    primary
-                    type="submit"
-                    />
-                  {form.error &&
-                  <span className="error-message">{form.error.message}</span>}
-                </fieldset>
-              </form>
+          <Row>
+            <Col sm={6} smOffset={3} xs={6} xsOffset={2}>
+              <div className="headline">
+                <h1>{msg.legend}</h1>
+                </div>
+              <Paper>
+                <div className="formWrapper" >
+                  <form onSubmit={::this.onFormSubmit}>
+                    <fieldset disabled={form.disabled}>
+                      <mui.TextField
+                        autoFocus
+                        hintText={msg.placeholder.email}
+                        fullWidth
+                        name="email"
+                        onChange={actions.setFormField}
+                        value={form.fields.email}/>
+                      <br />
+                      <mui.TextField
+                        hintText={msg.placeholder.password}
+                        name="password"
+                        fullWidth
+                        onChange={actions.setFormField}
+                        type="password"
+                        value={form.fields.password}/>
+                      <br />
+                      <mui.RaisedButton
+                        disabled={form.disabled}
+                        label={msg.button.login}
+                        primary
+                        type="submit"
+                        />
+                      {form.error &&
+                      <span className="error-message">{form.error.message}</span>}
+                    </fieldset>
+                  </form>
+                </div>
+              </Paper>
             </Col>
           </Row>
         </Grid>
