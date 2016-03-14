67d4a4877a35cc045bc7f10554ca3ad1b978510f
Sergii Gamaiunov <hello@webkadabra.com>
2016-03-15 00:08:23 +0200

    Tweak project dashboard page
    Add avatar, icon & description, remove development notes

--- src/client/projects/dashboard.js
+++ src/client/projects/dashboard.js
@@ -2,21 +2,29 @@
  * Brainfock - Community & Business Management Solution
  *
  * @link http://www.brainfock.org
- * @copyright Copyright (c) 2015 Sergii Gamaiunov <hello@webkadabra.com>
+ * @copyright Copyright (c) 2016 Sergii Gamaiunov <hello@webkadabra.com>
  */
 import React from 'react';
+import mui, {Styles, Avatar, IconButton} from 'material-ui';
+const Colors = Styles.Colors;
+
 export default class Dashboard extends React.Component{
 
   render()
   {
-    const {boards:{list, board, group}, topic_actions, msg, history} = this.props;
+    const {boards:{list, board: {data}, group}, topic_actions, msg, history} = this.props;
+    const color = Colors[data.logoBackground];
+    const icon = "fa "+data.logoIcon;
 
     return (
       <div className="wiki-wrapper">
         <div className="wiki-page">
           <div className="container-fluid">
             <div className="row">
-            Project <b>{board.summary}</b> dashboard is in development
+              <h1>
+                <Avatar icon={<span className={icon}/>} backgroundColor={color} /> {data.summary}
+              </h1>
+              <h3>{data.text}</h3>
             </div>
           </div>
         </div>
