f3d8e4f5e81878cfeb69ce7d262c66227e39078b
Sergii Gamaiunov <hello@webkadabra.com>
2016-06-13 21:25:54 +0300

    Add `topic-user` immutable record for users of topic (and topics of a user)

---
+++ src/common/topics/topic-user.js
@@ -0,0 +1,23 @@
+/**
+ * Brainfock, <http://www.brainfock.org>
+ *
+ * Copyright (C) 2015-present Sergii Gamaiunov <hello@webkadabra.com>
+ * All rights reserved.
+ *
+ * This source code is licensed under the GPL-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import {Record} from 'immutable';
+
+export default Record({
+  cid: '',
+  id: '',
+  topicId: '',
+  userId: '',
+  accessLevel: '',
+  activeYn: '',
+  assignedOn: '',
+  assignedByUserId: '',
+  starredYn: '',
+  email: '', // pseudo-field for topic member invintaiton form
+});
